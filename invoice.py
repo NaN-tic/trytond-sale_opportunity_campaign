8c63a47b061fba2dc8e986a880c4e5514c59d6a0
Albert Cervera i Areny <albert@nan-tic.com>
2021-12-16 16:37:01 +0100

    Allow linking as expenses invoice lines of posted invoices.

--- invoice.py
+++ invoice.py
@@ -8,3 +8,8 @@ class InvoiceLine(metaclass=PoolMeta):
     __name__ = 'account.invoice.line'
     sale_opportunity_campaign = fields.Many2One('sale.opportunity.campaign',
         'Campaign')
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls._check_modify_exclude.add('sale_opportunity_campaign')
